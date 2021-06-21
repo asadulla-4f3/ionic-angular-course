539762093e71f514c5e22173e1785bca4a116d03
asadullaSyed <sasadulla@incapsulate.com>
2021-06-21 12:54:03 +0530

    Added remaining changes

---
+++ helloAngular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { Todo1Component } from './todo1/todo1.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    Todo1Component,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ionic-angular-course/README.md
@@ -0,0 +1,10 @@
+# ionic-angular-course
+
+Course for ionic with angular using Capacitor or Cordova for creating Progressive Web apps and 
+mobile builds for ios and android.
+
+# Branches
+main - contains initial code for ionic-angular without any changes.
+master - clone of main.
+develop - clone of master.
+# master branch should be updated whenever new feature got updated in develop branch.
